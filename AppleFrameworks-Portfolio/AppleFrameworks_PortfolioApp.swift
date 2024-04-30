fadc4daa97eaa86b23d2b8ce2f7d0f1364c73083
Jacob Aust <jaust.413@gmail.com>
2024-04-29 23:00:22 -0700

    Initial Commit

---
+++ AppleFrameworks-Portfolio/AppleFrameworks_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_PortfolioApp.swift
+//  AppleFrameworks-Portfolio
+//
+//  Created by Jacob Aust on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
